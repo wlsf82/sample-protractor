0b74e34e12e82ea65323b3599d2eccdc57afb3d3
Walmyr Filho <limaesilva@qelp.com>
2020-04-29 15:49:42 +0200

    Use double quotes in the test file
    To keep the standard already used in the protractor.conf.js file.

--- sample.spec.js
+++ sample.spec.js
@@ -1,25 +1,25 @@
-const helper = require('protractor-helper');
+const helper = require("protractor-helper");
 
-describe('Sample test suite', () => {
-  beforeEach(() => browser.get(''));
+describe("Sample test suite", () => {
+  beforeEach(() => browser.get(""));
 
-  it('Sample test case', () => {
-    const footer = element(by.css('footer'));
+  it("Sample test case", () => {
+    const footer = element(by.css("footer"));
 
     helper.scrollToElement(footer);
   });
 
-  it('Another sample test case', () => {
-    const headerHeading = element(by.css('header h1'));
+  it("Another sample test case", () => {
+    const headerHeading = element(by.css("header h1"));
 
-    helper.waitForTextToBePresentInElement(headerHeading, 'Walmyr');
+    helper.waitForTextToBePresentInElement(headerHeading, "Walmyr");
   });
 
-  it('One more sample test case', () => {
+  it("One more sample test case", () => {
     expect(helper.isCurrentUrlDifferentFromBaseUrl()).toBe(false);
   });
 
-  it('Final sample test case', () => {
+  it("Final sample test case", () => {
     helper.setTimeout();
   });
 });
